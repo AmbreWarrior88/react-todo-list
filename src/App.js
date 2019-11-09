cd69da1131027bd456a85913e6123827b4310cc0
AmbreWarrior88 <56254199+AmbreWarrior88@users.noreply.github.com>
2019-11-09 16:05:09 +0100

    try to delete task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do list</h1>
+      <form
+        className="to-do"
+        onSubmit={event => {
+          event.preventDefault();
+          const newTasks = [...tasks];
+          newTasks.push(taskInput);
+          setTasks(newTasks);
+        }}
+      >
+        {tasks.map((element, index) => {
+          return (
+            <div className="task">
+              <span>
+                <button
+                  onClick={event => {
+                    event.preventDefault();
+
+                    // Tentative pour récupérer l'élément du tableau correspondant à la croix cliquée (ne fonctionne pas):
+                    tasks.splice({ index }, 1);
+                  }}
+                >
+                  ✕
+                </button>
+              </span>
+
+              <span key={index}>{element}</span>
+            </div>
+          );
+        })}
+        <input
+          id="to-do-input"
+          className="task-input"
+          type="text"
+          value={taskInput}
+          onChange={event => {
+            setTaskInput(event.target.value);
+          }}
+        />
+
+        <br></br>
+        <input className="submit" type="submit" value="Ajouter une tâche" />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
