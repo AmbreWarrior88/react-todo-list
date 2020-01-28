1d5b50ad13b32d44d9e3b27680bf748843740caf
AmbreWarrior88 <56254199+AmbreWarrior88@users.noreply.github.com>
2020-01-28 18:17:08 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -3,47 +3,59 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  // State to save user input
   const [taskInput, setTaskInput] = useState("");
+
+  // State to save the tasks
   const [tasks, setTasks] = useState([]);
+
+  const Task = (task, index) => {
+    return (
+      <li className="task">
+        <span
+          onClick={() => {
+            const newTasks = [...tasks];
+            newTasks.splice(index, 1);
+            setTasks(newTasks);
+          }}
+        >
+          𝘟{" "}
+        </span>
+        <span
+          className={task.isDone === true ? "done" : ""}
+          onClick={() => {
+            const newTasks = [...tasks];
+            newTasks[index].isDone = !newTasks[index].isDone;
+
+            setTasks(newTasks);
+          }}
+        >
+          {task.name}
+        </span>
+      </li>
+    );
+  };
 
   return (
     <div>
       <h1>To-Do list</h1>
+      <ul>{tasks.map(Task)}</ul>
+
       <form
         className="to-do"
         onSubmit={event => {
           event.preventDefault();
           const newTasks = [...tasks];
-          newTasks.push(taskInput);
+          newTasks.push({ name: taskInput, isDone: false });
           setTasks(newTasks);
           setTaskInput("");
         }}
       >
-        {tasks.map((element, index) => {
-          return (
-            <div className="task">
-              <span>
-                <button
-                  type="button"
-                  onClick={event => {
-                    event.preventDefault();
-                    const arrayToDeleteTask = [...tasks];
-                    arrayToDeleteTask.splice(index, 1);
-                    setTasks(arrayToDeleteTask);
-                  }}
-                >
-                  ✕
-                </button>
-              </span>
-
-              <span key={index}>{element}</span>
-            </div>
-          );
-        })}
         <input
           id="to-do-input"
           className="task-input"
           type="text"
+          placeholder="Titre"
           value={taskInput}
           onChange={event => {
             setTaskInput(event.target.value);
